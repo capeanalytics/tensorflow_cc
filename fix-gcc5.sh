fc8eaffccc65f1b07f0bc24f9402ce12f53a0c0b
Anton Brandl <anton@capeanalytics.com>
2018-06-01 14:40:27 +0200

    added script that fixes a common gcc5 bug, which prevents tensorflow from installing correctly.

---
+++ fix-gcc5.sh
@@ -0,0 +1,5 @@
+apt install -y curl
+
+for f in avx512fintrin.h avx512pfintrin.h avx512vlintrin.h; do
+   curl -H "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36" -o $f "https://gcc.gnu.org/viewcvs/gcc/branches/gcc-5-branch/gcc/config/i386/${f}?view=co&revision=245536&content-type=text%2Fplain&pathrev=245536"
+done && mv avx512*intrin.h  /usr/lib/gcc/x86_64-linux-gnu/5/include/
